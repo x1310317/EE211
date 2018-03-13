d072573ccf5985985bb09153f8fc59d25f619fe8
CallMeDDL <liuty1317@gmail.com>
2018-03-13 02:45:24 -0700

    Add html

---
+++ cGANcolor.js
@@ -0,0 +1,14 @@
+document.getElementById("dataset").onclick = function(){
+	document.getElementById("dataset-box").style.visibility = "visible";
+}
+document.getElementById("preproccess").onclick = function(){
+	document.getElementById("facial-box").style.visibility = "visible";
+	document.getElementById("edge-box").style.visibility = "visible";
+	document.getElementById("median-box").style.visibility = "visible";
+}
+document.getElementById("train").onclick = function(){
+	document.getElementById("train1-box").style.visibility = "visible";
+	document.getElementById("train2-box").style.visibility = "visible";
+	document.getElementById("train3-box").style.visibility = "visible";
+	document.getElementById("train4-box").style.visibility = "visible";
+}
