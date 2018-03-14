142acaa83919a7638cbbff6501fee282307e2b7f
CallMeDDL <liuty1317@gmail.com>
2018-03-13 23:22:36 -0700

    Almost finish demo

--- cGANcolor.js
+++ cGANcolor.js
@@ -1,3 +1,8 @@
+var select = document.getElementById("inputGroupSelect");
+var edgeimg = document.getElementById("edgeimg");
+var resimg = document.getElementById("resimg");
+var progress = document.getElementById("progressbtn");
+
 document.getElementById("dataset").onclick = function(){
 	document.getElementById("dataset-box").style.visibility = "visible";
 }
@@ -11,4 +16,42 @@ document.getElementById("train").onclick = function(){
 	document.getElementById("train2-box").style.visibility = "visible";
 	document.getElementById("train3-box").style.visibility = "visible";
 	document.getElementById("train4-box").style.visibility = "visible";
+}
+
+document.getElementById("stop").onclick = function(){
+	document.getElementById("resimg-box").style.visibility = "hidden";
+	progress.style.width = "0%";
+}
+
+document.getElementById("run").onclick = function(){
+	setTimeout(function(){
+		progress.style.width = "20%";
+		setTimeout(function(){
+			progress.style.width = "50%";
+			setTimeout(function(){
+				progress.style.width = "80%";
+				setTimeout(function(){
+					progress.style.width = "100%";
+					setTimeout(function(){
+						document.getElementById("resimg-box").style.visibility = "visible";
+					}, Math.floor(Math.random() * Math.floor(500) + 800));
+				}, Math.floor(Math.random() * Math.floor(500) + 800));
+			}, Math.floor(Math.random() * Math.floor(500) + 800));
+		}, Math.floor(Math.random() * Math.floor(500) + 800));
+	}, Math.floor(Math.random() * Math.floor(500) + 800));
+
+}
+select.onchange = function(){
+	if (select.options[select.selectedIndex].value == 1){
+		edgeimg.src = "test.png";
+		resimg.src = "test.png";
+		document.getElementById("resimg-box").style.visibility = "hidden";
+		progress.style.width = "0%";
+	}
+	if (select.options[select.selectedIndex].value == 2){
+		edgeimg.src = "2.jpg";
+		resimg.src = "2.jpg";
+		document.getElementById("resimg-box").style.visibility = "hidden";
+		progress.style.width = "0%";
+	}
 }
